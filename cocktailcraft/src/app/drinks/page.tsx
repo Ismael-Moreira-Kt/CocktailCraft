146b41f453049206510a6f9044bc3ec7b157638b
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-02-17 02:10:28 +0000

    API handling and automatic card creation.

--- cocktailcraft/src/app/drinks/page.tsx
+++ cocktailcraft/src/app/drinks/page.tsx
@@ -4,6 +4,7 @@ import { BackIcon } from "@/components/icons/back-icon";
 import styles from "./page.module.css";
 import { Card } from "@/components/card";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface DrinkPageProps {
     searchParams: (search: string) => void;
@@ -17,11 +18,31 @@ export default function DrinkPage({ searchParams }: DrinkPageProps) {
         router.push("/");
     }
 
-
     const apiUrl = searchParams.search === "random"
         ? "https://www.thecocktaildb.com/api/json/v1/1/random.php"
         : `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchParams.search}`;
 
+    const [drinks, setDrinks] = useState([]);
+    const [error, setError] = useState(null);
+
+    const loadDrinks = async () => {
+        try {
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error('Erro ao carregar os drinks');
+            }
+
+            const data = await response.json();
+            setDrinks(data.drinks || []);
+        } catch (error) {
+            setError(error.message);
+            router.push("/");
+        }
+    };
+    
+    useEffect(() => {
+        loadDrinks();
+    }, [searchParams]);
 
     return( 
         <main className={styles.main}>
@@ -34,9 +55,13 @@ export default function DrinkPage({ searchParams }: DrinkPageProps) {
             </header>
 
             <div className={styles.cardList}>
-                <Card imageUrl="https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg" title="155 Belmont"/>
-                <Card imageUrl="https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg" title="155 Belmont"/>
-                <Card imageUrl="https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg" title="155 Belmont"/>
+                {drinks.map((drink, index) => (
+                    <Card
+                        key={index}
+                        imageUrl={drink.strDrinkThumb}
+                        title={drink.strDrink}
+                    />
+                ))}
             </div>
         </main>
     )
